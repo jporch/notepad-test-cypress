3d802febb519368f7fb0141605d732cbae0f277d
Jesse M. Porch <porchjm@gmail.com>
2020-08-13 13:06:41 -0400

    building out tests a bit

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,18 +1,52 @@
+const testData = {
+    target: 'http://localhost:3000/',
+    header: 'Notepad',
+    note1: {
+        name: 'Jesse',
+        email: 'porchjm@gmail.com',
+        comment: 'Words about words about words'
+    },
+    note2: {
+        name: 'Testing',
+        email: 'test@gmail.com',
+        comment: 'Test test test test'
+    },
+}
+
+const postNote = (note) => {
+    cy.get('input[name="name"').type(note.name)
+    cy.get('input[name="email"').type(note.email)
+    cy.get('textarea[name="comment"').type(note.comment)
+    cy.get('button[type="submit"]').click()
+}
+
+const validateNote = (selector, data) => {
+    cy.get(selector).contains(`${data.name} (${data.email})`)
+    cy.get(selector).contains(`${data.comment}`)
+}
+
 describe('Basic pageload', () => {
     it('Visits Notepad and verifies that the page loaded', () => {
-      cy.visit('http://localhost:3000/')
-      cy.contains('Notepad')
+      cy.visit(testData.target)
+      cy.contains(testData.header)
     })
   })
 
-  describe('Add a note', () => {
+describe('Add a note', () => {
     it('Submits a new note', () => {
-      cy.visit('http://localhost:3000/')
-      cy.get('input[name="name"').type('Jesse')
-      cy.get('input[name="email"').type('porchjm@gmail.com')
-      cy.get('textarea[name="comment"').type('Words about words about words')
-      cy.get('button[type="submit"]').click()
-      cy.get('.notes-list div:first-child').contains('Jesse (porchjm@gmail.com)')
-      cy.get('.notes-list div:first-child').contains('Words about words about words')
+        cy.visit(testData.target)
+        postNote(testData.note1)
+        validateNote('.notes-list div:first-child',testData.note1)
     })
-  })
+})
+
+describe('Submit notes', () => {
+    it('Submits two new notes', () => {
+        cy.visit(testData.target)
+        postNote(testData.note1)
+        validateNote('.notes-list div:first-child',testData.note1)
+        postNote(testData.note2)
+        validateNote('.notes-list div:first-child',testData.note2)
+        validateNote('.notes-list div:nth-child(2)',testData.note1)
+    })
+})
